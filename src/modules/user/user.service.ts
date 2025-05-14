bae276203534de8f64c10ab5bf9fb23532512823
Victor Aponte <vaponte520@gmail.com>
2025-05-14 13:54:27 -0300

    feat: implement UserService with registration logic and password hashing

---
+++ src/modules/user/user.service.ts
@@ -0,0 +1,41 @@
+import { Injectable, ConflictException } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { User } from "./entities/user.entities";
+import { CreateUserDto } from "./dto/create-user.dto";
+import * as bcrypt from 'bcrypt';
+
+
+
+@Injectable()
+export class UserService {
+
+    constructor(
+        @InjectRepository(User)
+        private userRepository: Repository<User>
+    ) { }
+
+    async register(createUserDto: CreateUserDto) {
+        const { email, password, confirmPassword } = createUserDto;
+
+        const existingUser = await this.userRepository.findOne({ where: { email } })
+
+        if (existingUser) {
+            throw new ConflictException('El email ya está registrado');
+        }
+
+        if (password !== confirmPassword) {
+            throw new ConflictException('Las contraseñas no coinciden');
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = this.userRepository.create({
+            ...createUserDto,
+            password: hashedPassword
+        })
+
+        return this.userRepository.save(user)
+    }
+
+}
