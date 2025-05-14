ca095c0e000e40f72cd84c8581732297f1090ee3
Victor Aponte <vaponte520@gmail.com>
2025-05-14 13:54:10 -0300

    feat: configure TypeORM with MySQL and integrate UserModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './modules/user/user.module';
+import { User } from './modules/user/entities/user.entities';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      host: "localhost",
+      port: 3306,
+      username: 'root',
+      password: 'Admin123.',
+      database: 'salud360_db',
+      entities: [User],
+      synchronize: true,
+    }),
+    UserModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
