735c61a01be3a3f878508adac7f85bde167cce24
Victor Aponte <vaponte520@gmail.com>
2025-05-15 09:22:17 -0300

    feat: create Specialty entity for managing medical specialties

---
+++ src/modules/specialties/entities/specialty.entity.ts
@@ -0,0 +1,19 @@
+// src/modules/specialties/entities/specialty.entity.ts
+import { Entity, PrimaryGeneratedColumn, Column, ManyToMany } from 'typeorm';
+import { Doctor } from 'src/modules/doctors/entities/doctors.entities';
+
+
+@Entity()
+export class Specialty {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ unique: true })
+    name: string;
+
+    @Column({ nullable: true })
+    description: string;
+
+    @ManyToMany(() => Doctor, doctor => doctor.specialties)
+    doctors: Doctor[];
+}
