86cf1ae2b0d175ff538cfcc135010101d7bde408
Victor Aponte <vaponte520@gmail.com>
2025-05-16 12:17:41 -0300

    feat: implementar DoctorsController para gestionar operaciones relacionadas con doctores

---
+++ src/modules/doctors/doctor.controller.ts
@@ -0,0 +1,24 @@
+import { Controller, Get, Post, Body, Param, HttpCode, HttpStatus } from '@nestjs/common';
+import { DoctorsService } from './doctor.service';
+import { CreateDoctorDto } from './dto/create-doctor.dto';
+
+@Controller('doctors')
+export class DoctorsController {
+  constructor(private readonly doctorsService: DoctorsService) {}
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  create(@Body() createDoctorDto: CreateDoctorDto) {
+    return this.doctorsService.create(createDoctorDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.doctorsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.doctorsService.findOne(id);
+  }
+}
