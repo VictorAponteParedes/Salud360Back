56b06fd1e71ea119339e1f529ee9d5894d186b8e
Victor Aponte <vaponte520@gmail.com>
2025-05-16 12:20:37 -0300

    feat: agregar CreateDoctorDto para la creación de doctores con validaciones

---
+++ src/modules/doctors/dto/create-doctor.dto.ts
@@ -0,0 +1,42 @@
+import { IsArray, IsNotEmpty, IsOptional, IsString, IsNumber, IsDecimal } from '@nestjs/class-validator';
+
+
+export class CreateDoctorDto {
+  @IsNotEmpty()
+  @IsString()
+  firstName: string;
+
+  @IsNotEmpty()
+  @IsString()
+  lastName: string;
+
+  @IsOptional()
+  @IsDecimal()
+  rating?: number;
+
+  @IsOptional()
+  @IsNumber()
+  reviews?: number;
+
+  @IsOptional()
+  @IsString()
+  status?: string;
+
+  @IsOptional()
+  @IsString()
+  schedule?: string;
+
+  @IsOptional()
+  @IsString()
+  experience?: string;
+
+  @IsOptional()
+  @IsString()
+  description?: string;
+
+  @IsArray()
+  specialtyIds: string[];
+
+  @IsArray()
+  languageIds: string[];
+}
