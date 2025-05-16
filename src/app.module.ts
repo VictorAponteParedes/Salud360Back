90f33010ecb69c6b2debea6ecb1c7a0c2158b608
Victor Aponte <vaponte520@gmail.com>
2025-05-16 17:30:00 -0300

    fix: reorganizar importaciones y agregar ConfigModule y File entity en AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-//Modules
-import { TypeOrmModule } from '@nestjs/typeorm';
+// Modules
 import { UserModule } from './modules/user/user.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { LenguageModule } from './modules/lenguages/lenguage.module';
 import { SpecialtyModule } from './modules/specialties/specialty.module';
 import { DoctorsModule } from './modules/doctors/doctor.module';
+import { UploadModule } from './modules/file-upload/file.module';
 
-//Entities
+// Entities
 import { User } from './modules/user/entities/user.entities';
 import { Doctor } from './modules/doctors/entities/doctors.entities';
 import { Specialty } from './modules/specialties/entities/specialty.entity';
 import { Lenguages } from './modules/lenguages/entities/lenguages.entities';
+import { File } from './modules/file-upload/entities/file.entity';
 
 @Module({
   imports: [
+    // Configura ConfigModule primero
+    ConfigModule.forRoot({
+      isGlobal: true, // Esto hace que ConfigService esté disponible en todos los módulos
+    }),
     TypeOrmModule.forRoot({
       type: "mysql",
       host: "127.0.0.1",
@@ -25,14 +32,15 @@ import { Lenguages } from './modules/lenguages/entities/lenguages.entities';
       username: 'root',
       password: null,
       database: 'salud360_db',
-      entities: [User, Doctor, Specialty, Lenguages],
+      entities: [User, Doctor, Specialty, Lenguages, File],
       synchronize: true,
     }),
     UserModule,
     AuthModule,
     LenguageModule,
     SpecialtyModule,
-    DoctorsModule
+    DoctorsModule,
+    UploadModule
   ],
   controllers: [AppController],
   providers: [AppService],
