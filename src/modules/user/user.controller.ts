04743c28c8c9192960f7ed16f1a33b93dbf6228a
Victor Aponte <vaponte520@gmail.com>
2025-05-16 17:30:54 -0300

    feat: agregar soporte para carga de imagen de perfil en el registro de usuarios

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,17 +1,20 @@
-import { Post, Controller, Body } from "@nestjs/common";
-import { UserService } from "./user.service";
-import { CreateUserDto } from "./dto/create-user.dto";
-
-
+import { Controller, Post, Body, UseInterceptors, UploadedFile } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('users')
 export class UserController {
+  constructor(
+    private readonly userService: UserService
+  ) {}
 
-    constructor(private readonly userService: UserService) { }
-
-    @Post('register')
-    async register(@Body() createUserDto: CreateUserDto) {
-        return this.userService.register(createUserDto);
-    }
-
+  @Post('register')
+  @UseInterceptors(FileInterceptor('profileImage'))
+  async register(
+    @UploadedFile() file: Express.Multer.File,
+    @Body() createUserDto: CreateUserDto,
+  ) {
+    return this.userService.register(createUserDto, file);
+  }
 }
