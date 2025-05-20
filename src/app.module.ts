61dcb6a7960ac5be18eb857fd8136f24dceaa08d
Victor Aponte <vaponte520@gmail.com>
2025-05-20 12:31:33 -0300

    feat: add nodemailer dependency and update database host configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -26,10 +26,10 @@ import { File } from './modules/file-upload/entities/file.entity';
     }),
     TypeOrmModule.forRoot({
       type: "mysql",
-      host: "localhost",
+      host: "127.0.0.1",
       port: 3306,
       username: 'root',
-      password: 'Admin123.',
+      password: '',
       database: 'salud360_db',
       entities: [User, Doctor, Specialty, Lenguages, File],
       synchronize: true,
