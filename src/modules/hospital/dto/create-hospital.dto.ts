04f40c0a455841171297f4afc2b13cf7475b67c2
Victor Aponte <vaponte520@gmail.com>
2025-05-21 18:08:09 -0300

    feat: agregar DTO para la creación de hospitales

---
+++ src/modules/hospital/dto/create-hospital.dto.ts
@@ -0,0 +1,38 @@
+import { IsString, IsOptional, IsEmail, IsBoolean } from '@nestjs/class-validator';
+
+export class CreateHospitalDto {
+  @IsString()
+  name: string;
+
+  @IsString()
+  address: string;
+
+  @IsString()
+  city: string;
+
+  @IsString()
+  state: string;
+
+  @IsString()
+  country: string;
+
+  @IsOptional()
+  @IsString()
+  phone?: string;
+
+  @IsOptional()
+  @IsEmail()
+  email?: string;
+
+  @IsOptional()
+  @IsString()
+  website?: string;
+
+  @IsOptional()
+  @IsString()
+  hospitaImageId?: string;
+
+  @IsOptional()
+  @IsBoolean()
+  status?: boolean;
+}
