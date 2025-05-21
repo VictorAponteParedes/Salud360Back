dabf34094111d75d2404c173fd7fcae127302fc4
Victor Aponte <vaponte520@gmail.com>
2025-05-20 22:44:34 -0300

    feat: refactor sendResetPassword method to use HTML template for email content

--- src/modules/Email/email.service.ts
+++ src/modules/Email/email.service.ts
@@ -1,25 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { transporter } from 'src/config/nodemailer';
+import * as fs from 'fs';
+import * as path from 'path';
 
 @Injectable()
 export class EmailService {
   async sendResetPassword(email: string, token: string) {
-    
     const resetUrl = `http://localhost:3000/users/reset-password?token=${token}`;
-      console.log('Enviando correo a:', email, 'con url:', resetUrl);
-      console.log(`Preparando email para: ${email}`); // Log de depuración
-    
+    const templatePath = path.join(process.cwd(), 'src', 'modules', 'Email', 'templates', 'reset-password.html');
+    let html = fs.readFileSync(templatePath, 'utf8');
+    html = html.replace('{{resetUrl}}', resetUrl);
+
     try {
       const mailOptions = {
         from: '"Nombre de tu App" <vaponte520@gmail.com>',
         to: email,
         subject: 'Restablece tu contraseña',
-        html: `
-          <h1>Restablecimiento de contraseña</h1>
-          <p>Haz clic en el siguiente enlace para restablecer tu contraseña:</p>
-          <a href="${resetUrl}">Restablecer contraseña</a>
-          <p>Si no solicitaste este cambio, ignora este mensaje.</p>
-        `,
+        html,
       };
 
       const info = await transporter.sendMail(mailOptions);
