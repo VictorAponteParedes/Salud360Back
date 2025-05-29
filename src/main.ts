b865de4182cedc0a4a140f81a9f9474f587b5f0b
Victor Aponte <vaponte520@gmail.com>
2025-05-28 22:57:21 -0300

    feat: enable CORS for local development

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,13 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
+
   app.useStaticAssets(join(__dirname, '..', 'uploads'), {
     prefix: '/uploads/',
   });
