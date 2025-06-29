600ac7e54d9a5f7722ad6ef6eb06fed9c909fc1c
Victor Aponte <vaponte520@gmail.com>
2025-06-29 15:56:16 -0300

    Agrega: entidad InformationCard con propiedades y relaciones

---
+++ src/modules/serviceInfoCard/entities/service-information-card.entities.ts
@@ -0,0 +1,28 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, OneToOne, JoinColumn } from 'typeorm';
+import { File } from 'src/modules/file-upload/entities/file.entity';
+
+
+@Entity('information_cards')
+export class InformationCard {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column({ length: 100 })
+    title: string;
+
+    @Column({ type: 'text' })
+    description: string;
+
+    @OneToOne(() => File, { nullable: true })
+    @JoinColumn()
+    serviceImage: File;
+
+    @Column({ nullable: true })
+    screen: string;
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn()
+    updatedAt: Date;
+}
