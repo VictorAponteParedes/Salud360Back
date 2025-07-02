2c4f482deda8bd89b3cafcbef01b7fc8ff34f48b
Victor Aponte <vaponte520@gmail.com>
2025-07-02 11:18:03 -0300

    Corrige: configuración de CORS y mejora la inicialización del servidor

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const configService = app.get(ConfigService);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: configService.get('FRONTEND_URL') || 'http://localhost:5173',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
     allowedHeaders: 'Content-Type, Accept, Authorization',
@@ -17,7 +19,7 @@ async function bootstrap() {
     prefix: '/uploads/',
   });
 
-  await app.listen(process.env.PORT);
-  console.log(`🚀 App running on http://localhost:${process.env.PORT}`);
+  await app.listen(configService.get('PORT'));
+  console.log(`🚀 App running on ${configService.get('BASE_URL') || 'http://localhost'}:${configService.get('PORT')}`);
 }
 bootstrap();
