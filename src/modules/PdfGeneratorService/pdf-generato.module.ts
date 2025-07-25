c790864ec95cb29cfebb2abc55c9b0c6cbf64bf5
Victor Aponte <vaponte520@gmail.com>
2025-07-25 17:36:29 -0300

    refactor: remove unused PdfGeneratorModule and its service imports
    Clean up code by removing unused PdfGeneratorModule and its service imports from user module

--- src/modules/PdfGeneratorService/pdf-generato.module.ts
+++ src/modules/PdfGeneratorService/pdf-generato.module.ts
@@ -1,9 +1,9 @@
-import { Module } from '@nestjs/common';
-import { PdfGeneratorService } from './pdf-generator.service';
+// import { Module } from '@nestjs/common';
+// import { PdfGeneratorService } from './pdf-generator.service';
 
 
-@Module({
-    providers: [PdfGeneratorService],
-    exports: [PdfGeneratorService],
-})
-export class PdfGeneratorModule { }
+// @Module({
+//     providers: [PdfGeneratorService],
+//     exports: [PdfGeneratorService],
+// })
+// export class PdfGeneratorModule { }
--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -7,12 +7,12 @@ import { File } from "../file-upload/entities/file.entity";
 import { FileService } from "../file-upload/file.service";
 import { UploadModule } from "../file-upload/file.module";
 import { EmailService } from "../Email/email.service";
-import { PdfGeneratorService } from "../PdfGeneratorService/pdf-generator.service";
+// import { PdfGeneratorService } from "../PdfGeneratorService/pdf-generator.service";
 
 
 @Module({
     imports: [TypeOrmModule.forFeature([User, File]), UploadModule],
-    providers: [UserService, FileService, EmailService, PdfGeneratorService],
+    providers: [UserService, FileService, EmailService],
     controllers: [UserController],
     exports: [UserService],
 })
